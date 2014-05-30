fb9a69db422103673862c6b57bbe7841641d1f6c
Chormon <Chormon@Chormon_komp>
2014-05-30 15:19:00 +0200

    Removed unused imports, updated .gitignore

--- src/main/java/pl/chormon/elopvpranking/EloPVPRanking.java
+++ src/main/java/pl/chormon/elopvpranking/EloPVPRanking.java
@@ -23,11 +23,8 @@
  */
 package pl.chormon.elopvpranking;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.logging.Level;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
