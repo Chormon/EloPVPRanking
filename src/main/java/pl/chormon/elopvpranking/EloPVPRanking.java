9b4442c2f03c640132c14df8478ae12cf43b0f7f
Chormon <Chormon@Chormon_komp>
2014-07-04 15:55:34 +0200

    It's 1.0.4;

--- src/main/java/pl/chormon/elopvpranking/EloPVPRanking.java
+++ src/main/java/pl/chormon/elopvpranking/EloPVPRanking.java
@@ -40,7 +40,7 @@ import pl.chormon.utils.MsgUtils;
  */
 public class EloPVPRanking extends JavaPlugin {
 
-    private static final String version = "1.0.3";
+    private static final String version = "1.0.4";
 
     private static EloPVPRanking plugin;
     private EloFile eloFile;
